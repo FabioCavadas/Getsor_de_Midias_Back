47b2a05b54bb9dd6158e3292c3574e5cb3fa21fb
Fabio Cavadas Fernandes Ferreira <fabio.cavadas@g.globo>
2025-01-21 20:40:01 -0300

    atualização no formulário de usuário

--- user/serializers.py
+++ user/serializers.py
@@ -5,15 +5,13 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'password')
+        fields = ('id', 'username', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=validated_data['password'],
-            first_name=validated_data.get('first_name'),
-            last_name=validated_data.get('last_name'),
+            password=validated_data['password']
         )
         return user
