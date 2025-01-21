69b240c12258fc35624ce2e2d882222517b562e9
Fabio Cavadas Fernandes Ferreira <fabio.cavadas@g.globo>
2025-01-21 20:24:02 -0300

    add o campo category_name para exibição no filtro

--- midia/serializers.py
+++ midia/serializers.py
@@ -5,19 +5,27 @@ from .models import Category, Media
 
 class MediaSerializer(serializers.ModelSerializer):
     category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), required=True)
+    category_name = serializers.SerializerMethodField()
 
     class Meta:
         model = Media
-        fields = ["id", "name", "is_active", "category"]
+        fields = ["id", "name", "is_active", "category", "category_name", "media", "media_type"]
+
+    def get_category_name(self, obj):
+        return obj.category.name
 
 
 class MediaDetailSerializer(MediaSerializer):
     category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), required=True)
+    category_name = serializers.SerializerMethodField()
     is_active = serializers.BooleanField(required=False, default=True)
 
     class Meta:
         model = Media
-        fields = ["id", "name", "is_active", "category"]
+        fields = ["id", "name", "is_active", "category", "category_name", "media", "media_type"]
+
+    def get_category_name(self, obj):
+        return obj.category.name
 
     def deactivate_unmentioned_apps(self, existing_apps, requested_app_ids):
         for existing_app in existing_apps:
